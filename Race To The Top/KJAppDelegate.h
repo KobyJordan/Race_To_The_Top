f289e394f2b8a71463e5abbdb3d8cd7d7066608f
Koby Jordan <kobyjordan@Kobys-Mac.local>
2014-06-01 12:58:01 +1000

    Initial Commit

---
+++ Race To The Top/KJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KJAppDelegate.h
+//  Race To The Top
+//
+//  Created by Koby Jordan on 1/06/2014.
+//  Copyright (c) 2014 Koby Jordan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Race To The Top/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Race To The Top
+//
+//  Created by Koby Jordan on 1/06/2014.
+//  Copyright (c) 2014 Koby Jordan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KJAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KJAppDelegate class]));
+    }
+}
